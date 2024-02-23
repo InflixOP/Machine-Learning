aee04b6b194f5e1d56aa25fcaefe7d4caacb665b
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-02-23 21:54:14 +0530

    Update practpython2.py

--- practpython2.py
+++ practpython2.py
@@ -10,3 +10,12 @@ is_friend=False
 can_message="message allowed" if is_friend else "not allowed"
 print(can_message)
 '''
+#Short Circuiting
+
+'''
+is_friend=True
+is_user=True
+
+if is_friend and is_user:
+    print("Best friends forever")
+'''
