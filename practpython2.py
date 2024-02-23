594bd67fc87fb0e33560501f8ce696acac62011c
Anubhav Saxena <saxenaanubhav1204@gmail.com>
2024-02-23 21:52:13 +0530

    Update practpython2.py

--- practpython2.py
+++ practpython2.py
@@ -1,5 +1,12 @@
-is_old=False
-if is_old:
-    print("you are old")
-else:
-    print("not old") 
+# is_old=False
+# if is_old:
+#     print("you are old")
+# else:
+#     print("not old") 
+
+# Ternary operator
+'''
+is_friend=False
+can_message="message allowed" if is_friend else "not allowed"
+print(can_message)
+'''
